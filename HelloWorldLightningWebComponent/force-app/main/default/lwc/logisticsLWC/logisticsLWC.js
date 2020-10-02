8d73de2a269fae7eab8f46d689d5a77a0263d102
Swethasekar0723 <54064454+Swethasekar0723@users.noreply.github.com>
2020-10-02 21:58:58 +0530

    Update logisticsLWC.js

--- HelloWorldLightningWebComponent/force-app/main/default/lwc/logisticsLWC/logisticsLWC.js
+++ HelloWorldLightningWebComponent/force-app/main/default/lwc/logisticsLWC/logisticsLWC.js
@@ -3,10 +3,12 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import NAME_FIELD from '@salesforce/schema/Customer__c.Name';
 import PHONE_FIELD from '@salesforce/schema/Customer__c.Phone__c';
 import EMAIL_FIELD from '@salesforce/schema/Customer__c.Email__c';
-export default class LightningRecordFormCreateExampleLWC extends LightningElement {
-    // objectApiName is "Account" when this component is placed on an account record page
+import STREET_FIELD from '@salesforce/schema/Customer__c.Street__c';
+import CITY_FIELD from '@salesforce/schema/Customer__c.City__c';
+import PRODUCT_NAME_FIELD from '@salesforce/schema/Customer__c.Product_Name__c';
+export default class LightningRecordFormCreateExampleLWC extends LightningElement 
     @api objectApiName;
-    fields = [NAME_FIELD, PHONE_FIELD, EMAIL_FIELD];
+    fields = [NAME_FIELD, PHONE_FIELD, EMAIL_FIELD,STREET_FIELD,CITY_FIELD,PRODUCT_NAME_FIELD];
     handleSuccess(event) {
         const evt = new ShowToastEvent({
             title: 'Account created',
